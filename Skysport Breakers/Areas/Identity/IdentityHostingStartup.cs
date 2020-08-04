a58db839f442bac9dd0115e876b56cb1c5adff91
Diya Patel <ac94364@avcol.school.nz>
2020-08-05 09:23:06 +1200

    Add project files.

---
+++ Skysport Breakers/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Skysport_Breakers.Areas.Identity.Data;
+using Skysport_Breakers.Data;
+
+[assembly: HostingStartup(typeof(Skysport_Breakers.Areas.Identity.IdentityHostingStartup))]
+namespace Skysport_Breakers.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<AuthDbContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("AuthDbContextConnection")));
+
+                services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<AuthDbContext>();
+            });
+        }
+    }
+}
