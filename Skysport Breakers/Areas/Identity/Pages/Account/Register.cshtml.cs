3c2623213f6cb2df5a2ecef3cbb8a7f53c5e85a2
Diya Patel <ac94364@avcol.school.nz>
2020-08-05 09:50:11 +1200

    adding controls for first-name and last-name in user registration form and To save these property values to correspond table

--- Skysport Breakers/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Skysport Breakers/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -48,6 +48,16 @@ namespace Skysport_Breakers.Areas.Identity.Pages.Account
         {
             [Required]
             [EmailAddress]
+            [Display(Name = "First Name")]
+            public string FirstName { get; set; }
+
+            [Required]
+            [EmailAddress]
+            [Display(Name = "Last Name")]
+            public string LastName { get; set; }
+
+            [Required]
+            [EmailAddress]
             [Display(Name = "Email")]
             public string Email { get; set; }
 
@@ -75,7 +85,7 @@ namespace Skysport_Breakers.Areas.Identity.Pages.Account
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = Input.Email, Email = Input.Email };
+                var user = new ApplicationUser { UserName = Input.Email, Email = Input.Email, FirstName=Input.FirstName, LastName=Input.LastName };
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
