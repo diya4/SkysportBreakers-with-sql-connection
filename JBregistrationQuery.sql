07ad53b2031df1f000102f76c369e9f63107c31b
Diya Patel <ac94364@avcol.school.nz>
2020-10-20 18:54:39 +1300

    sql query and table created for Junior breaker registration created and Junior breaker registration controller added

---
+++ JBregistrationQuery.sql
@@ -0,0 +1,14 @@
+﻿create table [dbo].[JBregistration]
+(
+ID int identity (1,1) not null primary key,
+PName varchar(100),
+PEmail varchar(100),
+PAddress varchar(100),
+PPhoneNumber varchar(100),
+PHomeNumber varchar(100),
+CName varchar(100),
+CAge varchar(100),
+CDOB varchar(100),
+CGender char(1),
+SkillLevel varchar(100),
+);
